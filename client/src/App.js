ca60901bc1c668fa00d7b3f869b1bfd2e0a20b78
nitishjaiswal2002 <nitishjaiswal928@gmail.com>
2026-04-12 00:26:45 +0530

    change

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import UploadSection from "./components/UploadSection";
 import ResultsTable from "./components/ResultsTable";
 import Header from "./components/Header";
